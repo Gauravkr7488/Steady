6264c659b20ba50bb23e65eaefa11fc17cc123ee
Gaurav <gauravkr7488@gmail.com>
2026-08-19 13:01:03 +0530

    added new TxnCard.kt

--- app/src/main/java/com/example/steady/ui/screen/Home.kt
+++ app/src/main/java/com/example/steady/ui/screen/Home.kt
@@ -18,42 +18,28 @@ package com.example.steady.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.Tag
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.steady.SharedViewModel
 import com.example.steady.constant.Routes
+import com.example.steady.ui.component.card.TxnCard
 import com.steady.db.Tag
 import com.steady.db.Txn
 
@@ -91,76 +77,11 @@ fun HomeScreen(
                 data = txnList.map { it to sharedViewModel.getTags(it.id) }
             }
 
-            LazyColumn {
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(1.dp)) {
                 items(data, key = { it.first.id }) {
                     TxnCard(it.first, it.second)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun TxnCard(txn: Txn, tags: List<Tag>) {
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer
-        ),
-        shape = RoundedCornerShape(12.dp),
-        modifier = Modifier.padding(vertical = 1.dp) // spacer
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(top = 16.dp, bottom = 16.dp)
-            ) {
-                Row {
-                    Text(
-                        text = txn.title,
-                        style = MaterialTheme.typography.bodyLarge,
-                        overflow = TextOverflow.Ellipsis,
-                    )
-                    Spacer(Modifier.weight(0.5f))
-                    Text(
-                        text = txn.amount.toString(),
-                        style = MaterialTheme.typography.bodyLarge,
-                        overflow = TextOverflow.Ellipsis,
-                    )
-                }
-                Row(Modifier.height(16.dp)) {
-                    tags.forEach { tag ->
-                        Badge(tag.name)
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Badge(text: String) {
-    Row(
-        modifier = Modifier.width(80.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(4.dp)
-    ) {
-        Icon(
-            imageVector = Icons.Default.Tag,
-            contentDescription = "Tag",
-            modifier = Modifier.size(12.dp),
-            tint = MaterialTheme.colorScheme.onSecondaryContainer
-        )
-        Text(
-            text = text,
-            fontSize = 12.sp,
-            fontWeight = FontWeight.Medium,
-            color = MaterialTheme.colorScheme.onSecondaryContainer
-        )
     }
 }
