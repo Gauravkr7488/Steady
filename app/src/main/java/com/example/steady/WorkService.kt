81960703d1392643fdd7ce63a1d92b1db74047b6
Gaurav <gauravkr7488@gmail.com>
2026-09-07 15:34:04 +0530

    created WorkService.kt

---
+++ app/src/main/java/com/example/steady/WorkService.kt
@@ -0,0 +1,25 @@
+package com.example.steady
+
+import android.content.Context
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
+import java.util.concurrent.TimeUnit
+
+
+class WorkService(private val context: Context) {
+    fun scheduleWork(scheduleTime: Long, action: String) {
+        val delay = scheduleTime - System.currentTimeMillis()
+
+        if (delay <= 0) return
+
+        val data = workDataOf( "action" to action)
+
+        val request = OneTimeWorkRequestBuilder<SteadyWorker>()
+            .setInitialDelay(delay, TimeUnit.MILLISECONDS)
+            .setInputData(data)
+            .addTag("steady-$action")
+            .build()
+        WorkManager.getInstance(context).enqueue(request)
+    }
+}
